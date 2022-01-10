d5e46c49853d6ad3f3530a465b9a7a7f54b5735b
Gizem Güler <61242394+gizemguler@users.noreply.github.com>
2022-01-10 19:56:59 +0300

    first commit

---
+++ UdemyNLayerProject.Core/Services/ICategoryService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyNLayerProject.Core.Models;
+
+namespace UdemyNLayerProject.Core.Services
+{
+    public interface ICategoryService : IService<Category>
+    {
+        Task<Category> GetWithProductsByIdAsync(int categoryId);
+
+        //Category özgü methodlar varsa burada tanımlayabiliriz.
+    }
+}
---
+++ permissionManagement2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace permissionManagement2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
